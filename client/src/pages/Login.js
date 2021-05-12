4d69920de506a11d2f98df2c0af6ef978b919247
Aakashshinde <rajshindeslv@gmail.com>
2021-05-12 19:55:59 +0530

    gg

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -41,19 +41,18 @@ function Login(props){
              <img src="C:\Users\wasim\Desktop\Cyber\client\src\pages\Club LOGO_bg.png" width='60vh' height='100vh'/>
              </div>
              <h2>For H4ck3r5</h2>
-            <div>
-                {/*
+            <div> 
             <Grid className="panestyle">
             <Paper elevation={10} className="paperStyle">
             <Grid container spacing={0} justify='center' alignItems='center' direction='column'>
             <h2 >Sign In/Sign Up</h2>
             </Grid>
-            <TextField label='Username' placeholder='Enter Username' fullWidth required />
-            <TextField label='Password' placeholder='Enter Password' fullWidth required/>
-            <Button variant='contained' type='submit' color='primary' >Sign In</Button>
+            <TextField className="loginuser" label="Username" placeholder="username" name="username" type="text" value={values.username} onChange={onchange} error={errors.username ? true : false} fullWidth required />
+            <TextField className="loginpass" label="Password" placeholder="password" name="password" type="password" value={values.password} onChange={onchange} error={errors.password ? true : false}/ fullWidth required/>
+            <Button variant='contained' type='submit' color='primary' onClick={onSubmit} className={loading ? 'loading': ''}>Sign In</Button>
           </Paper>
         </Grid>
-                */}
+                {/*
             <Form onSubmit={onSubmit} className={loading ? 'loading': ''}>
                <h1 className="loginh1">Login</h1>
                <Form.Input  className="loginuser" label="Username" placeholder="username" name="username" type="text" value={values.username} onChange={onchange} error={errors.username ? true : false}/>
@@ -62,6 +61,7 @@ function Login(props){
                <Button class="text-center" type="submit" primary>Login</Button>
                    </center>
      </Form>
+                */}
            {Object.keys(errors).length>0 && (
             <div className="ui error message">
             <ul className="list">
